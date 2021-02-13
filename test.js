f262eabd08a8896e9a416f8f1ec3c6624ecc28fc
pandoragear <andy.dang.it@gmail.com>
2021-02-12 23:45:41 -0600

    Added separte test JS

---
+++ test.js
@@ -0,0 +1,11 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function muiltiply (num1, num2){
+    return num1 * num2;
+}
+
+function calculator (num1, num2, operator){
+    return operator(num1,num2);
+}
